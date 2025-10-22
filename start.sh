92f448be15b928bcf49c09f61bdbd5e244acb68b
ruizmarselo94-wq <ruizmarselo94@gmail.com>
2025-10-22 00:18:20 -0300

    Agregar start.sh para Railway
    .git/COMMIT_EDITMSG [unix] (00:28 22/10/2025)                                                                                          1,1 Top
                                                                                                                                         Agregar start.sh para Railway

--- start.sh
+++ start.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 cd backend
+python -m pip install --upgrade pip
 pip install -r requirements.txt
+if [ ! -f "dononofre.db" ]; then
+    python -m app.init_db
+fi
 uvicorn app.main:app --host 0.0.0.0 --port $PORT
